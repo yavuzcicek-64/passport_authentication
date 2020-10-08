93a2c2b2d233166bac92994b58102e563d68ca3d
Bayram Yavuz ÇİÇEK <bayramyavuzcicek@hotmail.com>
2020-10-08 16:42:57 +0300

    /users/registered  kullanıcı kayıt işlemleri ve bcrypt

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 /* ***************************LOGIN PAGE************************ */
 router.get('/login' , (req,res)=>{
     res.render('login');
@@ -43,7 +44,48 @@ router.post('/register' , (req,res)=>{
            password2
        });
    }else{
-       res.send('pass');
+       //Validation passed
+        User.findOne({email : email})
+        .then((user)=>{
+            if(user){
+                //User exist
+                errors.push({message : 'Email is already registered'});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            }else{
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                //hash password
+
+                bcrypt.genSalt(10, (err,salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err,hash)=>{
+                        if(err) throw err;
+                        // set password to hashed
+                        newUser.password = hash;
+
+                        newUser.save()
+                        .then((user)=>{
+                            res.redirect('/users/login');
+                        })
+                        .catch((err)=>{
+                            console.log(err);
+                        })
+                    });
+                })
+
+            }
+        })
+        .catch((err)=>{console.log(err)});
+
    }
 });
 
