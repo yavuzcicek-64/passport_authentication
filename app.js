7ecce78ffcfcfcd2cb28f9d7d75fe3f75ab0bdf2
Bayram Yavuz ÇİÇEK <bayramyavuzcicek@hotmail.com>
2020-10-08 15:50:46 +0300

    mongoDB bağlantısı

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const app = express();
 
-
+/* **************************DB CONFIG AND CONNECTION************************* */
+const db = require('./config/keys').MongoURI;
+mongoose.connect(db , {useNewUrlParser:true ,useUnifiedTopology : true})
+.then(()=>{
+    console.log('MONGO DB CONNECTED !'
+    )})
+.catch((err)=>{
+    console.log(err)
+});
+/* **************************************************************************** */
 
 /* **************************TEMPLATE ENGINE*********************************** */
 app.use(ejsLayouts);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// connection string
+
+module.exports = {
+    MongoURI : 'mongodb+srv://yavuz:yvzcck1994.@cluster.bnlgu.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
