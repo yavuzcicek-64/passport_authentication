7f9d3f3f8b5ea25b5b300baa0877de642800d793
Bayram Yavuz ÇİÇEK <bayramyavuzcicek@hotmail.com>
2020-10-08 16:18:55 +0300

    user model oluşturuldu validasyon işlemleri yapıldı

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,51 @@ const express = require('express');
 const router = express.Router();
 
 /* ***************************LOGIN PAGE************************ */
-router.get('/login' , (req,res,next)=>{
+router.get('/login' , (req,res)=>{
     res.render('login');
 });
 /* ************************************************************* */
 
 /* ************************REGISTER PAGE************************ */
-router.get('/register' , (req,res,next)=>{
+router.get('/register' , (req,res)=>{
     res.render('register');
 });
+/* ************************************************************* */
+
+/* ************************REGISTER HANDLE************************ */
+router.post('/register' , (req,res)=>{
+   const { name , email, password , password2 } = req.body;
+
+   let errors = [];
+
+   //Check required fields
+   if(!name || !email || !password || !password2){
+        errors.push({message : 'Please fill in all fields'});
+   }
+
+   //Check password match
+   if(password !== password2){
+       errors.push({message : 'Passwords do not matched'});
+   }
+
+   //Check password length
+   if(password.length < 6 ){
+        errors.push({message : 'Password should be at least 6 characters'});
+   }
+
+   if(errors.length > 0 ){
+       res.render('register', {
+           errors,
+           name,
+           email,
+           password,
+           password2
+       });
+   }else{
+       res.send('pass');
+   }
+});
+
 /* ************************************************************* */
 
 module.exports = router;
